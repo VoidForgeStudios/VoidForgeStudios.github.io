4f8ad8d83ef04eaab5c25d43f831c840c857ef54
SDev <Devime7289128912@outlook.com>
2026-09-04 23:38:14 +0200

    Refactor blocks.js for improved code formatting
    Formatted BLOCKS and HOTBAR for better readability.

--- minecraft/blocks.js
+++ minecraft/blocks.js
@@ -1,8 +1,30 @@
 export const BLOCKS = {
-  grass: { name:"Grass", color:0x59a83b, top:0x6fbd45, bottom:0x79502f },
-  dirt:  { name:"Dirt", color:0x79502f },
-  stone: { name:"Stone", color:0x888888 },
-  wood:  { name:"Wood", color:0x9a6a3a }
+  grass: {
+    name: "Grass",
+    color: 0x59a83b,
+    top: 0x6fbd45,
+    bottom: 0x79502f
+  },
+
+  dirt: {
+    name: "Dirt",
+    color: 0x79502f
+  },
+
+  stone: {
+    name: "Stone",
+    color: 0x888888
+  },
+
+  wood: {
+    name: "Wood",
+    color: 0x9a6a3a
+  }
 };
 
-export const HOTBAR = ["grass","dirt","stone","wood"];
+export const HOTBAR = [
+  "grass",
+  "dirt",
+  "stone",
+  "wood"
+];
